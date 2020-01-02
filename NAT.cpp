4db213f91feed1ba3064c4a0eba176b51e61c318
luckky_zhou <luckky_zhou@163.com>
2020-01-01 18:17:27 -0800

    add comment

--- NAT.cpp
+++ NAT.cpp
@@ -8,12 +8,17 @@ using namespace std;
 
 int main()
 {
-    map<string,string>Map;
-    char line1[10000];
-    char temp1[50];
-    string nat1, nat2;
+    // Read NAT.txt and store.
+    map<string,string>Map; // Use a map to store nat rules.
+    char line1[10000]; // Lines during read files.
+    char temp1[50]; // Define char to store file stream when read files. 
+    string nat1, nat2; // Temporary variebles to store string during read files.
 
     ifstream file1("NAT.txt");
+    if (file1.fail()) {
+        string error1 = "Unable to open NAT.txt";
+        throw invalid_argument(error1);
+    }; // Error throw.
     while (file1.good()) {
         file1.getline(line1,256);
         istringstream iss1(line1);
@@ -24,15 +29,20 @@ int main()
         token = strtok(NULL, ",");
         nat2 = token;
 
-        Map[nat1] = nat2;
+        Map[nat1] = nat2; // Store nat rules in map.
     }
 
+    // Read FLOW.txt and store.
     char line2[10000];
     char temp2[50];
-    char flow[10000][50];
+    char flow[10000][50]; // Use a char[] to store flows.
 
     int i = 0;
     ifstream file2("FLOW.txt");
+    if (file2.fail()) {
+        string error2 = "Unable to open FLOW.txt";
+        throw invalid_argument(error2);
+    }; // Error throw.
     while (file2.good()) {
         file2.getline(line2, 256);
         istringstream iss2(line2);
@@ -42,10 +52,12 @@ int main()
         i++;
     }
     
-    map<string,string>::iterator iter1, iter2, iter3;
+    // Match flow and nat rules. Give the output on console.
+    map<string,string>::iterator iter1, iter2, iter3; // Iterators when use find method to match.
     string ip, port, flow1, flow2;
     int j = 0;
     while (flow[j] != "") {
+        // Split ip and port of flow.
         char temp[50];
         strcpy(temp,flow[j]);
         char* token1 = strtok(temp, ":");
@@ -53,11 +65,13 @@ int main()
         token1 = strtok(NULL, ":");
         port = token1;
 
+        // Stitch ip with "*" and stitch "*" with port.
         flow1 = ip;
         flow1.append(":*");
         flow2 = "*:";
         flow2.append(port);
 
+        // Match IP:PORT, IP:*, *:PORT with nat rules.
         iter1 = Map.find(flow[j]);
         iter2 = Map.find(flow1);
         iter3 = Map.find(flow2);
